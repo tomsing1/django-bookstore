02b44130ae2ebbb31240c610ecce8ab6380acae5
Thomas Sandmann <sandmannt@tutamail.com>
2026-01-04 15:56:53 -0800

    added pages and tests

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Local
     "accounts",
+    "pages",
 ]
 
 AUTH_USER_MODEL = "accounts.CustomUser"
@@ -59,7 +60,7 @@ ROOT_URLCONF = "django_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
